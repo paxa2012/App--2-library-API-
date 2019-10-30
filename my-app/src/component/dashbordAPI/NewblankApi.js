e9b07283ba225a72fb43b9db740f7e6cbc6bc123
paxa2012 <32680173+paxa2012@users.noreply.github.com>
2019-10-30 21:24:40 +0700

    123

--- my-app/src/component/dashbordAPI/NewblankApi.js
+++ my-app/src/component/dashbordAPI/NewblankApi.js
@@ -12,14 +12,30 @@ class NewblankApi extends React.Component {
                                     let id = parseInt(this.props.match.params.id);
                                     if (api.id === id) {
                                         return (
+                                            api.id === 3 ?    
+                                            <div className="flex-container" key={index}>
+                                            {
+                                                api.database.map((d, i) => {
+                                                    return(
+                                                        <div className="col-md-4 col-sm-12" key={i}>
+                                                            <div className="card">
+                                                                <div className="card-head">{d.author.key}  </div>
+                                                                <div className="card-body">{d.timestamp}</div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div> 
+                                        :
                                             <div className="flex-container" key={index}>
                                                 {
                                                     api.database.map((d, i) => {
                                                         return(
                                                             <div className="col-md-4 col-sm-12" key={i}>
                                                                 <div className="card">
-                                                                    <div className="card-head">{d.title}{d.author.key}</div>
-                                                                    <div className="card-body">{d.value}{d.timestamp}<img className="responsive-img" src={`http://image.tmdb.org/t/p/original${d.poster_path}`} alt={d.title} /></div>
+                                                                    <div className="card-head">{d.title}  </div>
+                                                                    <div className="card-body">{d.value}<img className="responsive-img" src={`http://image.tmdb.org/t/p/original${d.poster_path}`} alt={d.title} /></div>
                                                                 </div>
                                                             </div>
                                                         )
